d3a36135380b6b36130d2ab388c6cf960cba8662
Arash <arashr@icloud.com>
2017-07-30 15:55:02 +0430

    Xcode project

---
+++ Jiq/Jiq.h
@@ -0,0 +1,19 @@
+//
+//  Jiq.h
+//  Jiq
+//
+//  Created by Arash on 7/30/17.
+//  Copyright © 2017 Taskulu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Jiq.
+FOUNDATION_EXPORT double JiqVersionNumber;
+
+//! Project version string for Jiq.
+FOUNDATION_EXPORT const unsigned char JiqVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Jiq/PublicHeader.h>
+
+
